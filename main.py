ed90b087328da412946957ca55366d80a6051c69
punithpunii <punithmurthy2021@gmail.com>
2024-12-30 15:41:26 +0530

    LastName Validation

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import re
-#UC-1 First name starts with Cap and has minimum 3 characters
-pattern = r"^[A-Z][A-Za-z]{2,}$"
+#UC-2 Last name starts with Cap and has minimum 3 characters
+
+def valid_name_check(name):
+    name_pattern = r"^[A-Z][A-Za-z]{2,}$"
+    if re.match(name_pattern, name):
+        print("Valid Name", name)
+    else:
+        print("Invalid Name")
+        exit()
+
 firstname = input("Enter the First Name:")
-if re.match(pattern, firstname):
-    print("Valid Firstname", firstname)
-else:
-    print("Invalid Firstname")
+valid_name_check(firstname) #UC-1 First name starts with Cap and has minimum 3 characters
+lastname = input("Enter the Last Name:")
+valid_name_check(lastname) #UC-2 Last name starts with Cap and has minimum 3 characters
