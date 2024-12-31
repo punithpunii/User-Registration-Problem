be8cd23503f4c8736467231da92f5e0e40554535
punithpunii <punithmurthy2021@gmail.com>
2024-12-31 12:41:30 +0530

    Phno Validation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
-#UC-3 A User need to enter a valid email (E.g. abc.xyz@bl.co.in)
+#UC-4 A User need to follow pre-defined Mobile Format (E.g. 91 9919819801)
 
-def valid_name_check(name):
+def valid_name_check(name): # Name Validation
     name_pattern = r"^[A-Z][A-Za-z]{2,}$"
     if re.match(name_pattern, name):
         print("Valid Name", name)
@@ -9,12 +9,20 @@ def valid_name_check(name):
         print("Invalid Name")
         exit()
 
-def valid_email_check(email):
+def valid_email_check(email): # Email Validation
     email_pattern = r"^[a-zA-Z0-9]+(\.[a-zA-Z0-9]*)?@[a-zA-Z]+\.[a-z]{2,}(\.[a-z]+)?$"
     if re.match(email_pattern, email):
         print("Valid Email Address", email)
     else:
         print("Invalid Email Address, valid email-> (E.g. abc.xyz@bl.co.in)")
+        exit()
+    
+def valid_phno_check(phno): # Phno Validation
+    phno_pattern = r"^[0-9]{2} [0-9]{10}" 
+    if re.match(phno_pattern, phno):
+        print("Valid Phone Number", phno)
+    else:
+        print("Invalid Phone Number, valid Phone Number-> (E.g. 91 9919819801)")
         exit()
 
 firstname = input("Enter the First Name:")
@@ -22,4 +30,6 @@ valid_name_check(firstname) #UC-1 First name starts with Cap and has minimum 3 c
 lastname = input("Enter the Last Name:")
 valid_name_check(lastname) #UC-2 Last name starts with Cap and has minimum 3 characters
 email = input("Enter your Email Address:")
-valid_email_check(email)
+valid_email_check(email) #UC-3 A User need to enter a valid email (E.g. abc.xyz@bl.co.in)
+phno = input("Enter your Phone Number:")
+valid_phno_check(phno)
