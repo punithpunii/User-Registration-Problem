c21aaaa5b61d5d420e90df7549ed28b94ec6a9b3
punithpunii <punithmurthy2021@gmail.com>
2024-12-31 12:22:42 +0530

    Email Validation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-#UC-2 Last name starts with Cap and has minimum 3 characters
+#UC-3 A User need to enter a valid email (E.g. abc.xyz@bl.co.in)
 
 def valid_name_check(name):
     name_pattern = r"^[A-Z][A-Za-z]{2,}$"
@@ -9,7 +9,17 @@ def valid_name_check(name):
         print("Invalid Name")
         exit()
 
+def valid_email_check(email):
+    email_pattern = r"^[a-zA-Z0-9]+(\.[a-zA-Z0-9]*)?@[a-zA-Z]+\.[a-z]{2,}(\.[a-z]+)?$"
+    if re.match(email_pattern, email):
+        print("Valid Email Address", email)
+    else:
+        print("Invalid Email Address, valid email-> (E.g. abc.xyz@bl.co.in)")
+        exit()
+
 firstname = input("Enter the First Name:")
 valid_name_check(firstname) #UC-1 First name starts with Cap and has minimum 3 characters
 lastname = input("Enter the Last Name:")
 valid_name_check(lastname) #UC-2 Last name starts with Cap and has minimum 3 characters
+email = input("Enter your Email Address:")
+valid_email_check(email)
